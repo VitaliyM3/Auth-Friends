4630baf4438d14fabe31b50e7acb9c851962d248
Vitaliy Mikhaylyyk <vitaliymikhaylyuk@gmail.com>
2020-02-18 22:24:18 -0500

    login works, need to set routes

---
+++ friends/src/components/FriendsList.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {axiosWithAuth} from "../utils/axiosWithAuth";
+
+
+class FriendsList extends React.Component {
+    state = {
+        friends: []
+    };
+
+    // componentDidMount() {
+    //     this.getData();
+    // }
+
+    // getData = () => {
+    //     axiosWithAuth()
+    //         .get()
+    //         .then()
+    // };
+
+
+
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default FriendsList;
